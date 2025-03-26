c7c7f65db613f925dd47ebdfe18b3823cae22288
Mariana Resende <mariananresende@gmail.com>
2025-03-26 09:25:49 -0300

    atualizando o script

--- temperature.py
+++ temperature.py
@@ -43,7 +43,7 @@ def get_coordinates_from_city(city_name):
 
 # ========= Função: Consulta clima real por coordenadas =========
 def get_weather_by_coordinates(lat, lon):
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}&units=metric&lang=pt"
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric&lang=pt"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
